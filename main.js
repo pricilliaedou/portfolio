05c75cfc85287c9b1ca872cdc9d6d8516144c6b3
pricillia <pmoussavou@gmail.com>
2024-07-30 17:09:48 +0200

    dark mode

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
-var hamburger = document.getElementById("hamburger");
+let hamburger = document.getElementById("hamburger");
 // var container = document.getElementById("container");
 hamburger.addEventListener("click", open);
 function open() {
   navlink.classList.toggle("open");
+}
+
+let changeMode = document.getElementById("switch");
+let container = document.getElementById("container");
+
+// let navDark = document.getElementById("nav");
+changeMode.addEventListener("click", darkMode);
+function darkMode() {
+  container.classList.toggle("dark");
+  nav.classList.toggle("navDark");
 }
