d4a0c85d74053caabcea9b76ea3b240eb678a816
pricillia <pmoussavou@gmail.com>
2024-08-11 23:18:46 +0200

    skills et parcours

--- main.js
+++ main.js
@@ -14,3 +14,35 @@ function darkMode() {
   container.classList.toggle("dark");
   nav.classList.toggle("navDark");
 }
+
+// Timeline
+(function () {
+  // Sélection des li de la class timeline
+  let items = document.querySelectorAll(".timeline li");
+
+  // On vérifie si un élément est dans la fenêtre de visualisation
+
+  function isElementInViewport(el) {
+    let rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
+  function callbackFunc() {
+    for (var i = 0; i < items.length; i++) {
+      if (isElementInViewport(items[i])) {
+        items[i].classList.add("in-view");
+      }
+    }
+  }
+
+  //On écoute les différents évènements
+  window.addEventListener("load", callbackFunc);
+  window.addEventListener("resize", callbackFunc);
+  window.addEventListener("scroll", callbackFunc);
+})();
